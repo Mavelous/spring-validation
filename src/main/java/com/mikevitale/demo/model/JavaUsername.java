d056e269f4218428d2097a158bb52f1f037921de
Mike Vitale <github@mikevitale.com>
2021-03-06 05:58:35 -0600

    De-Lombok-ified the code (JavaUsername).

--- src/main/java/com/mikevitale/demo/model/JavaUsername.java
+++ src/main/java/com/mikevitale/demo/model/JavaUsername.java
@@ -1,26 +1,34 @@
 package com.mikevitale.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class JavaUsername {
 	@Pattern(regexp = "[A-Za-z]+", message = "Username Pattern Validation Message")
 	@Size(min = 2, max = 15, message = "Username Size Validation Message")
 	@Valid
 	public String username;
 
+	public JavaUsername() {}
+
+	public JavaUsername(String username) {
+		setUsername(username);
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
 	public void setUsername(String username) {
 		System.out.printf("Username = [%s]%n", username);
 		this.username = username;
+	}
+
+	@Override
+	public String toString() {
+		return "JavaUsername{" +
+		       "username='" + username + '\'' +
+		       '}';
 	}
 }
