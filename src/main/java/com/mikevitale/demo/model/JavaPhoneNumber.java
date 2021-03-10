43bf6e4dc1fe96b2773b3da65cfd0eb21e85d93e
Mike Vitale <github@mikevitale.com>
2021-03-10 08:45:31 -0600

    Added another model object.

---
+++ src/main/java/com/mikevitale/demo/model/JavaPhoneNumber.java
@@ -0,0 +1,14 @@
+package com.mikevitale.demo.model;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class JavaPhoneNumber {
+	@Pattern(regexp = "^1[0-9]{3}[0-9]{3}[0-9]{4}$", message = "Phone Number Pattern Validation Message")
+	@Size(min = 11, max = 11, message = "Phone Number Size Validation Message")
+	private String phoneNumber;
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
---
+++ src/test/java/com/mikevitale/demo/model/JavaPhoneNumberTest.java
@@ -0,0 +1,90 @@
+package com.mikevitale.demo.model;
+
+import java.util.Objects;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.util.AssertionErrors.fail;
+
+public class JavaPhoneNumberTest {
+	Validator validator;
+
+	private JavaPhoneNumber phoneNumber;
+
+	@BeforeEach
+	public void setUp() {
+		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+		validator = validatorFactory.getValidator();
+		phoneNumber = new JavaPhoneNumber();
+	}
+
+	@Test
+	public void phoneNumberShouldBeCorrectLength() {
+		phoneNumber.setPhoneNumber("11234567890");
+		Set<ConstraintViolation<JavaPhoneNumber>> violations = validator.validate(phoneNumber);
+
+		printViolations(violations);
+
+		assertEquals(0, violations.size());
+	}
+
+	@Test
+	public void phoneNumberShouldBeCorrectPattern() {
+		phoneNumber.setPhoneNumber("12234567890");
+		Set<ConstraintViolation<JavaPhoneNumber>> violations = validator.validate(phoneNumber);
+
+		printViolations(violations);
+
+		assertEquals(0, violations.size());
+	}
+
+	@Test
+	public void shoutsWhenLengthTooShort() {
+		phoneNumber.setPhoneNumber("1");
+		Set<ConstraintViolation<JavaPhoneNumber>> violations = validator.validate(phoneNumber);
+
+		printViolations(violations);
+
+		assertEquals(2, violations.size());
+		violations.stream().filter(v -> v.getMessage().contains("Pattern")).findAny().ifPresentOrElse(a -> {}, () -> fail("Message not found"));
+		violations.stream().filter(v -> v.getMessage().contains("Size")).findAny().ifPresentOrElse(a -> {}, () -> fail("Message not found"));
+	}
+
+	@Test
+	public void shoutsWhenLengthTooLong() {
+		phoneNumber.setPhoneNumber("123456789012");
+		Set<ConstraintViolation<JavaPhoneNumber>> violations = validator.validate(phoneNumber);
+
+		printViolations(violations);
+
+		assertEquals(2, violations.size());
+		violations.stream().filter(v -> v.getMessage().contains("Pattern")).findAny().ifPresentOrElse(a -> {}, () -> fail("Message not found"));
+		violations.stream().filter(v -> v.getMessage().contains("Size")).findAny().ifPresentOrElse(a -> {}, () -> fail("Message not found"));
+	}
+
+	@Test
+	public void shoutsWhenPatternNotMatchedButCorrectSize() {
+		phoneNumber.setPhoneNumber("abcdefghijk");
+		Set<ConstraintViolation<JavaPhoneNumber>> violations = validator.validate(phoneNumber);
+
+		printViolations(violations);
+
+		assertEquals(1, violations.size());
+		assertEquals("Phone Number Pattern Validation Message",
+				Objects.requireNonNull(violations.stream().findFirst().orElse(null)).getMessage());
+	}
+
+	private void printViolations(Set<ConstraintViolation<JavaPhoneNumber>> violations) {
+		for (ConstraintViolation<JavaPhoneNumber> violation : violations) {
+			System.out.printf("***** ==> [%s]%n", violation.getMessage());
+		}
+	}
+}
