80f82eeca2016edb759366eab845c65795dff0c5
Mike Vitale <github@mikevitale.com>
2021-03-16 07:30:06 -0500

    Added clarifying comment for @field annotations on KotlinUsername

--- src/main/kotlin/com/mikevitale/demo/model/KotlinUsername.kt
+++ src/main/kotlin/com/mikevitale/demo/model/KotlinUsername.kt
@@ -3,6 +3,8 @@ package com.mikevitale.demo.model
 import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
+// An explanation of @field:Pattern and @field:Size in Kotlin code:
+// https://stackoverflow.com/questions/36515094/kotlin-and-valid-spring-annotation
 data class KotlinUsername(
 		@field:Pattern(regexp = "[A-Za-z]+", message = "Username Pattern Validation Message")
 		@field:Size(min = 2, max = 15, message = "Username Size Validation Message")
