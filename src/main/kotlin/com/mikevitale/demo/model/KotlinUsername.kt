5ef65ac0b6cf3670c2f40a405ebd7e7069b181fe
Mike Vitale <github@mikevitale.com>
2021-03-16 07:57:02 -0500

    Removed redundant data class body.

--- src/main/kotlin/com/mikevitale/demo/model/KotlinUsername.kt
+++ src/main/kotlin/com/mikevitale/demo/model/KotlinUsername.kt
@@ -9,4 +9,4 @@ data class KotlinUsername(
 		@field:Pattern(regexp = "[A-Za-z]+", message = "Username Pattern Validation Message")
 		@field:Size(min = 2, max = 15, message = "Username Size Validation Message")
 		val username: String
-) {}
+)
