87236e2db2873dfd6a0229c0bc24d92663373f6e
Mike Vitale <github@mikevitale.com>
2021-03-16 07:56:14 -0500

    Added KotlinPhoneNumber and tests.

---
+++ src/test/kotlin/com/mikevitale/demo/model/KotlinPhoneNumberTest.kt
@@ -0,0 +1,67 @@
+package com.mikevitale.demo.model
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import javax.validation.ConstraintViolation
+import javax.validation.Validation
+import javax.validation.Validator
+import javax.validation.ValidatorFactory
+
+class KotlinPhoneNumberTest {
+	lateinit var validator: Validator
+	lateinit var phoneNumber: KotlinPhoneNumber
+
+	@BeforeEach
+	fun setup() {
+		val factory: ValidatorFactory = Validation.buildDefaultValidatorFactory()
+		validator = factory.validator
+	}
+
+	@Test
+	fun phoneNumberShouldBeCorrectLengthAndPattern() {
+		phoneNumber = KotlinPhoneNumber("11234567890")
+		val violations = validator.validate(phoneNumber)
+
+		printViolations(violations)
+
+		assertEquals(0, violations.size)
+	}
+
+	@Test
+	fun shoutsWhenLengthTooShort() {
+		phoneNumber = KotlinPhoneNumber("1")
+		val violations = validator.validate(phoneNumber)
+		printViolations(violations)
+		assertEquals(2, violations.size)
+		assertTrue(violations.any { it.message.equals("Phone Number Pattern Validation Message") })
+		assertTrue(violations.any { it.message.equals("Phone Number Size Validation Message") })
+	}
+
+	@Test
+	fun shoutsWhenLengthTooLong() {
+		phoneNumber = KotlinPhoneNumber("123456789012")
+		val violations = validator.validate(phoneNumber)
+		printViolations(violations)
+		assertEquals(2, violations.size)
+		assertTrue(violations.any { it.message.equals("Phone Number Pattern Validation Message") })
+		assertTrue(violations.any { it.message.equals("Phone Number Size Validation Message") })
+	}
+
+	@Test
+	fun shoutsWhenPatternNotMatchedButCorrectSize() {
+		phoneNumber = KotlinPhoneNumber("abcdefghijk")
+		val violations = validator.validate(phoneNumber)
+		printViolations(violations)
+		assertEquals(1, violations.size)
+		assertTrue(violations.any { it.message.equals("Phone Number Pattern Validation Message") })
+	}
+
+	private fun printViolations(violations: Set<ConstraintViolation<KotlinPhoneNumber>>) {
+		violations.forEach {
+			System.out.printf("***** ==> [%s]%n", it.message)
+		}
+	}
+
+}
