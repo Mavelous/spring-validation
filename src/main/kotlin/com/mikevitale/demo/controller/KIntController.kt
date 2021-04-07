980ee08bea9d33b4090abedac0c6ca9929d6dac7
Mike Vitale <github@mikevitale.com>
2021-04-07 10:03:01 -0500

    Added Kotlin Int controller, with some @Disabled tests.

---
+++ src/main/kotlin/com/mikevitale/demo/controller/KIntController.kt
@@ -0,0 +1,26 @@
+package com.mikevitale.demo.controller
+
+import mu.KotlinLogging
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+
+private val logger = KotlinLogging.logger {}
+
+@RestController
+@Validated
+@RequestMapping("/kotlin/")
+open class KIntController {
+	@GetMapping("int/{id}")
+	@ResponseBody
+	fun validateIntPathVariable(
+			@PathVariable("id") id:
+			@Min(value = 5, message = "A minimum value of 5 is required")
+			@Max(value = 9999, message = "A maximum value of 9999 can be given") Int)
+	: ResponseEntity<String>? {
+		logger.info { String.format("validatePathVariable(), Got id = %d", id) }
+		return ResponseEntity.ok("ID is valid")
+	}
+}
---
+++ src/test/kotlin/com/mikevitale/demo/controller/KIntControllerTest.kt
@@ -0,0 +1,97 @@
+package com.mikevitale.demo.controller
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.ResultMatcher
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@WebMvcTest(KIntController::class)
+class KIntControllerTest(@Autowired private val mvc: MockMvc) {
+	@Test
+	fun validIntegerPathVariable() {
+		val request: MockHttpServletRequestBuilder = givenARequestFor("/kotlin/int/5")
+		val actions: ResultActions = whenTheRequestIsMade(request)
+		thenExpect(actions,
+				MockMvcResultMatchers.status().isOk,
+				MockMvcResultMatchers.content().bytes("ID is valid".toByteArray()))
+	}
+
+	@Test
+	@Disabled(value = "Currently returning 200, not 400.")
+	fun shoutsWhenIntegerPathVariableIsTooLow() {
+		val request = givenARequestFor("/kotlin/int/4")
+		val resultActions = whenTheRequestIsMade(request)
+		val response = """{
+    "validationErrors": [
+        {
+            "fieldName": "validateIntPathVariable.id",
+            "message": "A minimum value of 5 is required"
+        }
+    ]
+}"""
+		val content = MockMvcResultMatchers.content()
+		thenExpect(resultActions,
+				MockMvcResultMatchers.status().isBadRequest,
+				content.contentType(MediaType.APPLICATION_JSON),
+				content.json(response))
+	}
+
+	@Test
+	@Disabled(value = "Currently returning 200, not 400.")
+	fun shoutsWhenIntegerPathVariableIsTooHigh() {
+		val request = givenARequestFor("/kotlin/int/10000")
+		val resultActions = whenTheRequestIsMade(request)
+		val response = """{
+    "validationErrors": [
+        {
+            "fieldName": "validateIntPathVariable.id",
+            "message": "A maximum value of 9999 can be given"
+        }
+    ]
+}"""
+		val content = MockMvcResultMatchers.content()
+		thenExpect(resultActions,
+				MockMvcResultMatchers.status().isBadRequest,
+				content.contentType(MediaType.APPLICATION_JSON),
+				content.json(response))
+	}
+
+	@Test
+	fun shoutsWhenStringIsPassedToIntegerPathVariable() {
+		val request = givenARequestFor("/kotlin/int/wegyrguywery")
+		val resultActions = whenTheRequestIsMade(request)
+		val response = """{
+    "validationErrors": [
+        {
+            "fieldName": "id",
+            "message": "Failed to convert value of type 'java.lang.String' to required type 'int'; nested exception is java.lang.NumberFormatException: For input string: \"wegyrguywery\""
+        }
+    ]
+}"""
+		val content = MockMvcResultMatchers.content()
+		thenExpect(resultActions,
+				MockMvcResultMatchers.status().isBadRequest,
+				content.contentType(MediaType.APPLICATION_JSON),
+				content.json(response))
+	}
+
+	private fun givenARequestFor(url: String): MockHttpServletRequestBuilder {
+		return MockMvcRequestBuilders.get(url)
+				.characterEncoding("UTF-8")
+	}
+
+	private fun whenTheRequestIsMade(request: MockHttpServletRequestBuilder): ResultActions {
+		return mvc.perform(request)
+	}
+
+	private fun thenExpect(resultActions: ResultActions, vararg matchers: ResultMatcher) {
+		resultActions.andExpect(ResultMatcher.matchAll(*matchers))
+	}
+}
